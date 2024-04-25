0082b56c20ab50ae1dc70dddd98728d99b894dfd
Mark Fynes <tfynes@vmware.com>
2024-04-25 16:23:10 -0400

    .

--- SCG/cf-service-2-routeconfig.sh
+++ SCG/cf-service-2-routeconfig.sh
@@ -5,11 +5,11 @@ fi
 cf_service_fqdn=$1
 cf_space=$2
 cf_k8s_ns=$(cf curl /v3/spaces | jq -r ".resources[] | select(.name==\"$cf_space\")" | jq -r .guid)
-echo "cf_k8s_ns = $cf_k8s_ns"
+#echo "cf_k8s_ns = $cf_k8s_ns"
 cf_k8s_service_guid=$(cf curl /v3/routes | jq -r ".resources[] | select(.url==\"$cf_service_fqdn\")" | jq -r .destinations[0].guid)
-echo "cf_k8s_service_guid=$cf_k8s_service_guid"
+#echo "cf_k8s_service_guid=$cf_k8s_service_guid"
 cf_k8s_service="s-$cf_k8s_service_guid"
-echo "cf_k8s_service=$cf_k8s_service"
+#echo "cf_k8s_service=$cf_k8s_service"
 
 curl -v --request POST http://localhost:18080/api/convert/openapi -H 'Content-Type: application/json' --data-raw "{
   \"service\": {
@@ -26,3 +26,4 @@ curl -v --request POST http://localhost:18080/api/convert/openapi -H 'Content-Ty
     }
   ]
 }" | jq -r .  
+
